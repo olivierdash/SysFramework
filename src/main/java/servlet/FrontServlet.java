bf373490d695ada037ff56e4d9ca40d616380f7b
olivierdash <otahiriniaina@gmail.com>
2026-06-21 16:15:37 +0300

    L'annotation peut desormais etre appele

---
+++ src/main/java/servlet/FrontServlet.java
@@ -0,0 +1,15 @@
+package main.java.servlet;
+
+import java.util.List;
+
+public class FrontServlet {
+    private List<String> listClass;
+
+    public List<String> getListClass() {
+        return listClass;
+    }
+
+    public void setListClass(List<String> listClass) {
+        this.listClass = listClass;
+    }
+}
